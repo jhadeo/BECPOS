11d1ff63b50bc508d43b3de519050da07dc0e799
jhadeo <105715821+jhadeo@users.noreply.github.com>
2025-06-05 10:30:56 +0800

    test page and route

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <h1>Hello React!</h1>;
+
+  return <a href ="./test" >Hello React!</a>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
